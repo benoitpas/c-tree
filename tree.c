3d939cb6c5c9c321eb982e617d72e6e1d01ac26e
benoitpas <bpasquereau@gmail.com>
2023-01-12 20:56:59 +0000

    procedure to print tree

--- tree.c
+++ tree.c
@@ -11,13 +11,45 @@ int countNodes(struct tree *t) {
     }
     return r;
 }
+ 
+void printTree(struct tree *t)
+{
+    printf("(");
+    if (t->value != NULL) 
+    {
+        printf("%s", (char*) t->value);
+    }
+    if (t->left != NULL)
+    {
+        printf(",");
+        printTree(t->left);
+    }
+    if (t->right != NULL)
+    {
+        printf(","); 
+        printTree(t->right);
+    }
+    printf(")");
+};
 
 #ifndef UNIT_TEST
 int main() {
-    struct tree test;
-    
-   /* printf() displays the string inside quotation */
-   printf("Hello, World!");
+    struct tree testa;
+    struct tree testb;
+    struct tree testc;
+    struct tree testd;
+    struct tree teste;
+    testa.value = "A";
+    testa.left = &testb;
+    testa.right = &testc;
+    testb.value = "B";
+    testc.value = "C";
+    testc.left = &testd;
+    testc.right = &teste;
+    testd.value = "D";
+    teste.value = "E";
+
+    printTree(&testa);     
    return 0;
 }
 #endif
